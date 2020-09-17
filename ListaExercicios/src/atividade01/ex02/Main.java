3ef4de7123dedde6e256c8bf1683455d82257369
CaiqueVidal <caique.freitas2@fatec.sp.gov.br>
2020-09-17 16:49:13 -0300

    Add Atividade01

---
+++ ListaExercicios/src/atividade01/ex02/Main.java
@@ -0,0 +1,111 @@
+package atividade01.ex02;
+
+import javax.swing.JOptionPane;
+
+import atividade01.LinkedList;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		LinkedList<Double> temperatureList = new LinkedList<>();
+		int opc = 0;
+		
+//		Simulações
+//		Double t1 = 31.5;
+//		Double t2 = 21.7;
+//		Double t3 = 27.9;
+//		Double t4 = 19.4;
+//		Double t5 = 13.2;
+//		
+//		temperatureList.addEnd(t1);
+//		temperatureList.addStart(t2);
+//		temperatureList.addEnd(t3);
+//		temperatureList.add(t4, 3);
+//		temperatureList.addStart(t5);
+//		JOptionPane.showMessageDialog(null, temperatureList.showLinkedList());
+//		temperatureList.remove(4);
+//		temperatureList.removeStart();
+//		temperatureList.removeEnd();
+//		JOptionPane.showMessageDialog(null, temperatureList.showLinkedList());
+		
+		while (opc != 9) {
+			opc = Integer.parseInt(JOptionPane.showInputDialog("Escolha uma opção: "
+					+ "\n1-Verifica lista vazia \n2-Adicionar no início \n3-Adicionar no final"
+					+ "\n4-Adicionar na posição \n5-Remover do início \n6-Remover do final "
+					+ "\n7-Remover da posição \n8-Mostrar lista \n9-Sair"));
+			
+			Double temperature;
+			
+			switch (opc) {
+			case 1:	
+				if (temperatureList.isEmpty()) {
+					JOptionPane.showMessageDialog(null,"Lista vazia!");
+				} else {
+					JOptionPane.showMessageDialog(null,"A lista não está vazia!");
+				}
+				break;
+				
+			case 2:
+				temperature = Double.parseDouble(JOptionPane.showInputDialog("Digite a temperatura: "));
+				temperature = temperatureList.addStart(temperature);
+				if (temperature != null) {
+					JOptionPane.showMessageDialog(null, "Temperatura \"" + temperature + "\" adicionada no início com sucesso");
+				}
+				break;
+				
+			case 3:
+				temperature = Double.parseDouble(JOptionPane.showInputDialog("Digite a temperatura: "));
+				temperature = temperatureList.addEnd(temperature);
+				if (temperature != null) {
+					JOptionPane.showMessageDialog(null, "Temperatura \"" + temperature + "\" adicionada no final com sucesso");
+				}
+				break;
+			
+			case 4:
+				temperature = Double.parseDouble(JOptionPane.showInputDialog("Digite a temperatura: "));
+				int posA = Integer.parseInt(JOptionPane.showInputDialog("Posição para inserir: "));
+				temperature = temperatureList.add(temperature, posA);
+				if (temperature != null) {
+					JOptionPane.showMessageDialog(null, "Temperatura \"" + temperature + "\" adicionada na posição " + posA);
+				}
+				break;
+				
+			case 5:
+				temperature = temperatureList.removeStart();
+				if (temperature != null) {
+					JOptionPane.showMessageDialog(null, "Temperatura \"" + temperature + "\" removida do início");
+				}
+				break;
+				
+			case 6:
+				temperature = temperatureList.removeEnd();
+				if (temperature != null) {
+					JOptionPane.showMessageDialog(null, "Temperatura \"" + temperature + "\" removida do final");
+				}
+				break;
+				
+			case 7:
+				int posR = Integer.parseInt(JOptionPane.showInputDialog("Posição para remover: "));
+				temperature = temperatureList.remove(posR);
+				if (temperature != null) {
+					JOptionPane.showMessageDialog(null, "Temperatura \"" + temperature + "\" removida da posição " + posR);
+				}
+				break;
+				
+			case 8:
+				JOptionPane.showMessageDialog(null, temperatureList.showLinkedList());
+				break;
+				
+			case 9:
+				JOptionPane.showMessageDialog(null,"Encerrando...");
+				System.exit(0);
+
+			default:
+				JOptionPane.showMessageDialog(null,"Opção inválida");
+			}
+			
+		}
+		
+	}
+	
+}
---
+++ ListaExercicios/src/atividade01/ex03/Main.java
@@ -0,0 +1,103 @@
+package atividade01.ex03;
+
+import javax.swing.JOptionPane;
+
+import atividade01.LinkedList;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		LinkedList<Student> list = new LinkedList<>();
+		int opc = 0;
+		
+//		Simulações
+//		Student st1 = new Student(7, "Caique", "18-2T", "5 °");
+//		Student st2 = new Student(3, "Victor", "18-1T", "6 °");
+//		Student st3 = new Student(15, "Pedro", "19-2N", "3 °");
+//		Student st4 = new Student(12, "Raizer", "19-1N", "3 °");
+//		Student st5 = new Student(23, "Gustavo", "20-2T", "1 °");
+//		
+//		list.addEnd(st1);
+//		list.addStart(st2);
+//		list.addEnd(st3);
+//		list.add(st4, 3);
+//		list.addStart(st5);
+//		JOptionPane.showMessageDialog(null, list.showLinkedList());
+//		list.remove(4);
+//		list.removeStart();
+//		list.removeEnd();
+//		JOptionPane.showMessageDialog(null, list.showLinkedList());
+		
+		while (opc != 9) {
+			opc = Integer.parseInt(JOptionPane.showInputDialog("Escolha uma opção: "
+					+ "\n1-Adicionar no final \n2-Adicionar no início"
+					+ "\n3-Adicionar na posição \n4-Remover do fim"
+					+ "\n5-Remover do início \n6-Remover da posição \n7-Mostrar lista \n9-Sair"));
+			
+			Student student = new Student();
+			
+			switch (opc) {
+			case 1:	
+				student.addStudent();
+				student = list.addEnd(student);
+				if (student != null) {
+					JOptionPane.showMessageDialog(null, "Aluno " + student.getName() +" adicionado no final com sucesso");
+				}
+				break;
+				
+			case 2:
+				student.addStudent();
+				student = list.addStart(student);
+				if (student != null) {
+					JOptionPane.showMessageDialog(null, "Aluno " + student.getName() +" adicionado no início com sucesso");
+				}
+				break;
+				
+			case 3:
+				student.addStudent();
+				int posA = Integer.parseInt(JOptionPane.showInputDialog("Posição para inserir: "));
+				student = list.add(student, posA );
+				if (student != null) {
+					JOptionPane.showMessageDialog(null, "Aluno " + student.getName() +" adicionado na posição "+ posA +" com sucesso");
+				}
+				break;
+			
+			case 4:
+				student = list.removeEnd();
+				if (student != null) {
+					JOptionPane.showMessageDialog(null, "Aluno " + student.getName() +" removido do final");
+				}
+				break;
+				
+			case 5:
+				student = list.removeStart();
+				if (student != null) {
+					JOptionPane.showMessageDialog(null, "Aluno " + student.getName() +" removido do início");
+				}
+				break;
+				
+			case 6:
+				int posR = Integer.parseInt(JOptionPane.showInputDialog("Posição para remover: "));
+				student = list.remove(posR);
+				if (student != null) {
+					JOptionPane.showMessageDialog(null, "Aluno " + student.getName() +" removido da posição " + posR);
+				}
+				break;
+				
+			case 7:
+				JOptionPane.showMessageDialog(null, list.showLinkedList());
+				break;
+				
+			case 9:
+				JOptionPane.showMessageDialog(null,"Encerrando...");
+				System.exit(0);
+
+			default:
+				JOptionPane.showMessageDialog(null,"Opção inválida");
+			}
+			
+		}
+		
+	}
+	
+}
